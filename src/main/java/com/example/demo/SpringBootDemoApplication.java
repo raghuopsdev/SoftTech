b10657c47f4d3aa9494c2bdab0977dfafb489937
Abinash <abinash.38vsv@gmail.com>
2020-12-22 22:53:35 +0530

    Dependency Injection

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -2,12 +2,40 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import com.example.demo.models.User;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringBootDemoApplication.class, args);
+		for (String bean : ctx.getBeanDefinitionNames())
+		{
+			System.out.println("Bean ---->"+bean);
+		}
 	}
 
+	@Bean
+	@Primary //Used as third type of resolving
+	public User getAdminUser()
+	{
+		//construct admin user by getting details from DB. 
+		User adminuser = new User("Abinash", "abinash@gmail.com", 6738901L);
+		return adminuser;
+				
+	}
+	
+	@Bean
+	public User getITUser()
+	{
+		//construct admin user by getting details from DB.
+		User ITuser = new User("IT", "abinash-IT@gmail.com", 6738901L);
+		return ITuser;
+				
+	}
+	
 }
--- src/main/java/com/example/demo/restcontroller/HomeController.java
+++ src/main/java/com/example/demo/restcontroller/HomeController.java
@@ -1,9 +1,15 @@
 package com.example.demo.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.*;
+
+//HomeController homeController = new HomeController()
+//homeController.setAdminUser = getadminUser from App contx
+//put homecontroller in app context 
 
 @RestController
 public class HomeController 
@@ -14,13 +20,76 @@ public class HomeController
 		return "Hello";
 	}
 	
-	@GetMapping("/user")
-	public User getUser()
-	{
-		User newuser = new User("Abinash", "abinash@gmail.com", 6738901L);
-		System.out.println(newuser);
-		return newuser;
-		
-	}
+	/* FIRST METHOD of resolving conflict [Resolving by type (here type = "User")]
+	 * Rename the variable of User type i.e, "User getITUser"  
+	 * with the bean name i.e,  getAdminUser or getITUser).
+	 * Below we are using the User getITUser */
+	
+//	
+//	  @Autowired 
+//	  public User getITUser;
+//	  
+//	  
+//	  @GetMapping("/user") 
+//	  public User getUser() 
+//	  {
+//	    System.out.println(getITUser); 
+//	    return getITUser; 
+//	  }
+	 
+	
+	/*FIRST METHOD ENDS */
+	
+	
+	/*SECOND METHOD [Resolving by name]*/
+	
+//	  @Autowired 
+//	  public User getITUser;
+//	  
+//	  @GetMapping("/it-user") 
+//	  public User getITUser() 
+//	  { 
+//	    System.out.println(getITUser);
+//	    return getITUser; 
+//	  }
+//	  
+//	  @Autowired 
+//	  public User getAdminUser;
+//	  
+//	  @GetMapping("/admin-user") 
+//	  public User getAdminUser() 
+//	  { 
+//	    System.out.println(getAdminUser);
+//	    return getAdminUser; 
+//	  }
+//	  
+	  
+	/*THIRD METHOD [Making one of the bean as primary]*/
+	  
+//	  @Autowired 
+//	  public User asdf;
+//	  
+//	  
+//	  @GetMapping("/user") 
+//	  public User getUser() 
+//	  {
+//	    System.out.println(asdf); 
+//	    return asdf; 
+//	  }
+	
+	/*Fourth METHOD, By Qualifiers*/
+	
+	  @Autowired 
+	  @Qualifier("getITUser")
+	  public User myITUser;
+	  
+	  
+	  @GetMapping("/it-user") 
+	  public User getUser() 
+	  {
+	    System.out.println(myITUser); 
+	    return myITUser; 
+	  }
+	
 }
 
