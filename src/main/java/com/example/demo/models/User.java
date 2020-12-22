c0de565beab777613b6f3b0550979f4ed7fa22f0
Abinash <abinash.38vsv@gmail.com>
2020-12-22 13:58:55 +0530

    first commit

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,34 @@
+package com.example.demo.models;
+
+public class User 
+{
+	private String name;
+	private String email;
+	private Long phoneNo;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(Long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public User(String name, String email, Long phoneNo) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.phoneNo = phoneNo;
+	} 
+	
+	
+}
---
+++ src/main/java/com/example/demo/restcontroller/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.*;
+
+@RestController
+public class HomeController 
+{
+	@GetMapping("/")
+	public String hello()
+	{
+		return "Hello";
+	}
+	
+	@GetMapping("/user")
+	public User getUser()
+	{
+		User newuser = new User("Abinash", "abinash@gmail.com", 6738901L);
+		System.out.println(newuser);
+		return newuser;
+		
+	}
+}
+
